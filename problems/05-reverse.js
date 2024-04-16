50ef75056ad3015ce588a25645748f5387eff45c
Laiba Afzal <laiba.a1298@gmail.com>
2024-04-16 15:44:46 -0400

    05

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -15,7 +15,18 @@ reverse(""); // ""
 
 function reverse(str) {
   // Your code here 
+  if (!str.length) return "";
+  
+  return reverse(str.slice(1)) + str[0];
 }
+
+
+console.log(reverse("house")); // "esuoh"
+// reverse("dog"); // "god"
+// reverse("atom"); // "mota"
+// reverse("q"); // "q"
+// reverse("id"); // "di"
+// console.log(reverse("")); // ""
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
